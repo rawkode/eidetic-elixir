192a39c0b152c69affb78c36342f5cc62a2a9ea1
David McKay <david@rawkode.com>
2017-03-03 13:56:44 +0000

    Added docker-compose.yml for simpler testing
    Signed-off-by: David McKay <david@rawkode.com>

--- mix.exs
+++ mix.exs
@@ -1,13 +1,16 @@
 defmodule Eidetic.Mixfile do
   use Mix.Project
 
+
   def project do
     [ app: :eidetic,
       version: "0.0.1",
       elixir: "~> 1.3",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
-      deps: deps()
+      deps: deps(),
+      aliases: aliases(),
+      elixirc_paths: elixirc_paths(Mix.env)
     ]
   end
 
@@ -21,5 +24,17 @@ defmodule Eidetic.Mixfile do
   def deps do
     []
   end
+
+  def aliases do
+    [
+      "init": ["local.hex --force", "deps.get"],
+      "test": ["init", "test"]
+    ]
+  end
+
+  defp elixirc_paths(:test),  do: ["lib","examples"]
+  defp elixirc_paths(:dev),   do: elixirc_paths(:test)
+  defp elixirc_paths(_),      do: []
+
 end
 
