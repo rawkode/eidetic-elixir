2be84ea9c42cfcb88628ded6d9d0c4007d01a5cb
David McKay <david@rawkode.io>
2017-04-13 11:45:01 +0100

    Removing redundant application module and bumping version (#18)

--- mix.exs
+++ mix.exs
@@ -1,10 +1,9 @@
 defmodule Eidetic.Mixfile do
   use Mix.Project
 
-
   def project do
     [ app: :eidetic,
-      version: "0.0.6",
+      version: "0.1.0",
       elixir: "~> 1.3",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
